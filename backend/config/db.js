e047508f600ba0905f7c02dc3c22e6ad287efeec
Aswin Ramesh <>
2024-05-18 15:44:49 +0530

    Integrated user login and registration endpoints with JSON Web Token authentication and input validation using validator.js in the backend.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 import mongoose, { mongo } from "mongoose";
-import dotenv from 'dotenv'
-dotenv.config()
+// import dotenv from 'dotenv'
+// dotenv.config()
 
 export const connectDB = async () => {
   await mongoose
     .connect(
       process.env.MONGODB_CON
     )
-    .then(() => console.log("DB Connected"));
+    .then(() => console.log("Connected to MongoDB Atlas"));
 };
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config'
 
 //App config
 const app = express();
@@ -15,14 +17,14 @@ app.use(cors());
 connectDB();
 
 //API endpoints
-app.use("/api/food", foodRouter)
-app.use("/images", express.static('uploads'))
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
 });
 
 app.listen(port, () => {
-  console.log(`Server is runnning on http://localhost:${port}`);
+  console.log(`Server is now running at http://localhost:${port}`);
 });
-
